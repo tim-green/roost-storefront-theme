b3ebf9aaca0b7160d25f7bfc914994d5dfa7c1a8
Tim <github@timgreen.ws>
2019-10-27 22:11:13 +1100

    added footer credit function

--- functions.php
+++ functions.php
@@ -29,9 +29,41 @@ function sf_child_theme_dequeue_style() {
 }
 
 
-
 /**
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
 
+//Roost Footer Credit is here
+if ( ! function_exists( 'storefront_credit' ) ) {
+    /**
+     * Display the theme credit
+     *
+     * @since  1.0.0
+     * @return void
+     */
+    function storefront_credit() {
+        $links_output = '';
+
+        if ( apply_filters( 'storefront_credit_link', true ) ) {
+            $links_output .= '<a href="https://timgreen.ws" target="_blank" title="' . esc_attr__( 'WooCommerce - The Best eCommerce Platform for WordPress', 'storefront' ) . '" rel="author">' . esc_html__( 'Built with Roost &amp; WooCommerce', 'storefront' ) . '</a>.';
+        }
+
+        if ( apply_filters( 'storefront_privacy_policy_link', true ) && function_exists( 'the_privacy_policy_link' ) ) {
+            $separator = '<span role="separator" aria-hidden="true"></span>';
+            $links_output = get_the_privacy_policy_link( '', ( ! empty( $links_output ) ? $separator : '' ) ) . $links_output;
+        }
+        
+        $links_output = apply_filters( 'storefront_credit_links_output', $links_output );
+        ?>
+        <div class="site-info">
+            <?php echo esc_html( apply_filters( 'storefront_copyright_text', $content = '&copy; ' . get_bloginfo( 'name' ) . ' ' . date( 'Y' ) ) ); ?>
+
+            <?php if ( ! empty( $links_output ) ) { ?>
+                <br />
+                <?php echo wp_kses_post( $links_output ); ?>
+            <?php } ?>
+        </div><!-- .site-info -->
+        <?php
+    }
+}
 
